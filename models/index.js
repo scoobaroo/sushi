a559998b1ce070179e8ca9fa76db1b476000e6f1
scoobaroo <scoobaroo415@gmail.com>
2016-04-12 10:01:59 -0700

    try to fix mongoose env vars

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.connect( process.env.MONGODB_URI ||
-                  process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
                   'mongodb://localhost/sushi' );
 
 var Restaurant = require('./restaurant');
