d5d3711c1831775593a1b7ec77ecc0b3d20ab451
scoobaroo <scoobaroo415@gmail.com>
2016-04-12 09:55:19 -0700

    add new config for mongo

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGOLAB_URI ||
+mongoose.connect( process.env.MONGODB_URI ||
+                  process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   'mongodb://localhost/sushi' );
 
